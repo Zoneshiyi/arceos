33af89cc6999024108594f20cef35805d1a474e6
zoneshiyi <zoneshiyi@163.com>
2024-12-03 21:18:47 +0800

    exercise 4

--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -3,7 +3,10 @@
 #![feature(asm_const)]
 
 #[cfg(feature = "axstd")]
-use axstd::println;
+use axstd::{
+    println,
+    process::exit,
+};
 
 const PLASH_START: usize = 0xffff_ffc0_2200_0000;
 
@@ -29,6 +32,29 @@ struct AppHeader {
     app_size: usize,
 }
 
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_EXIT: usize = 3;
+
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe { ABI_TABLE[num] = handle; }
+}
+
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
+fn abi_exit() {
+    println!("[ABI:Exit] Exit!");
+    exit(0);
+}
+
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
 
@@ -50,18 +76,34 @@ fn main() {
 
         let run_code = unsafe {from_raw_parts_mut(offset_of_exec_zone as *mut u8, app_size.min(MAX_APP_SIZE))};
         run_code.copy_from_slice(app_in_image);
-        println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+        println!("code {:?}; address [{:?}]", run_code, run_code.as_ptr());
 
         offset_of_image += app_size;
         offset_of_exec_zone += app_size;
     }
 
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+    register_abi(SYS_EXIT, abi_exit as usize);
+
     println!("Execute app ...");
+
+    let arg0= b'A';
     unsafe { core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        la      t1, {abi_table}
+        add     t1, t1, t0
+        ld      t1, (t1)
+        jalr    t1
         li      t2, {run_start}
         jalr    t2
         j       .",
         run_start = const RUN_START,
+        abi_table = sym ABI_TABLE,
+        //abi_num = const SYS_HELLO,
+        abi_num = const SYS_EXIT,
+        in("a0") arg0,
     )}
 
     println!("Load payload ok!");
